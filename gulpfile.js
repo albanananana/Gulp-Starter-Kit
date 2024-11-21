360ae62bc8d92fd40e591eda0a2db48ebd5cca7a
albanananana <albakhtsin.vlad@gmail.com>
2024-11-21 20:56:52 +0100

    Autoprefixer deleted

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
-const { Version } = require('sass');
 
 function scripts() {
-	return src('app/js/main.js')
+	return src([
+		'node_modules/swiper/swiper-bundle.js',
+		'app/js/main.js'
+	])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(dest('app/js'))
@@ -18,7 +19,6 @@ function scripts() {
 
 function styles() {
 	return src('app/scss/style.scss')
-		,pipe(autoprefixer({ overrideBrowserslist: ['last 10 version']}))
 		.pipe(concat('style.min.css'))
 		.pipe(scss({ outputStyle: 'compressed' }))
 		.pipe(dest('app/css'))
